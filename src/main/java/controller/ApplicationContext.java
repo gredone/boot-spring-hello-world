02f38d3d304f69fad2fe07d4a6f494c26189e1c0
AFIF Redouane (SPC/ENS) <redouane.afif@proximus.com>
2018-07-27 11:42:05 +0200

    push project

---
+++ src/main/java/controller/ApplicationContext.java
@@ -0,0 +1,15 @@
+package controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationContext {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationContext.class, args);
+		
+
+	}
+
+}
